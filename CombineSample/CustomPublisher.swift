e01ff7e8f938bebce728f86ce86a439b1b46aecf
marha-hwang <hwangharam@naver.com>
2025-01-22 23:17:17 +0900

    feat. 커스텀 구현

--- CombineSample/CustomPublisher.swift
+++ CombineSample/CustomPublisher.swift
@@ -6,3 +6,88 @@
 //
 
 import Foundation
+import Combine
+
+// 커스텀 Publisher 클래스
+class CustomPublisher: Publisher {
+    
+    typealias Output = String
+    typealias Failure = Never
+    
+    var value: Output {
+        didSet{
+            subscriptions.forEach{
+                $0.value = value
+                $0.request(.max(1))
+            }
+        }
+    }
+    private var subscriptions:[CustomSubscription] = []
+    
+    init(value: Output) {
+        self.value = value
+    }
+    
+    // 구독자가 이 Publisher를 구독할 때 호출되는 메서드
+    func receive<S>(subscriber: S) where S : Subscriber, S.Input == Output, S.Failure == Failure {
+        
+        guard let customSubscriber = subscriber as? CustomSubscriber else {
+            return
+        }
+        
+        let subscription = CustomSubscription(subscriber: customSubscriber, value: self.value)
+        subscriptions.append(subscription)
+        subscriber.receive(subscription: subscription)
+    }
+}
+
+// 구독자 구현
+class CustomSubscriber: Subscriber, Cancellable {
+    
+    typealias Input = String
+    typealias Failure = Never
+    
+    private var subscription: Subscription?
+    
+    func receive(subscription: Subscription) {
+        self.subscription = subscription
+        subscription.request(.max(1))  // 한 번만 값 요청
+    }
+    
+    func receive(_ input: Input) -> Subscribers.Demand {
+        print("Received value: \(input)")
+        return .none
+    }
+    
+    func receive(completion: Subscribers.Completion<Failure>) {
+        print("Completion received.")
+    }
+    
+    func cancel() {
+        subscription?.cancel()
+    }
+}
+
+// 구독자에게 값을 발행하는 Subscription 클래스
+class CustomSubscription:Subscription{
+    
+    typealias T = String
+    
+    private var subscriber:CustomSubscriber?
+    var value:T
+    
+    init(subscriber: CustomSubscriber, value:T) {
+        self.subscriber = subscriber
+        self.value = value
+    }
+    
+    func request(_ demand: Subscribers.Demand) {
+        _ = subscriber?.receive(value)
+    }
+    
+    func cancel() {
+        subscriber?.receive(completion: .finished)
+        subscriber = nil
+        
+    }
+}
--- CombineSample/main.swift
+++ CombineSample/main.swift
@@ -6,6 +6,17 @@
 //
 
 import Foundation
+import Combine
 
-print("Hello, World!")
+// 커스텀 Publisher 사용 예시
+let publisher = CustomPublisher(value: "구독을 시작했어요~~")
+let subscriber = CustomSubscriber()
+
+publisher.receive(subscriber: subscriber)
+publisher.value = "구독 메시지를 발행했어요~"
+
+
+publisher.sink(receiveValue: {_ in }).cancel()
+
+publisher.value = "구독을 취소했어요~"
 
